db1fdc1abd35907a5fcf460cb810d14b4c27feb7
nooitaf <nooitaf@gmail.com>
2016-03-10 06:19:36 +0100

    wooh rainbow working :P

--- examples/squarerainbow.js
+++ examples/squarerainbow.js
@@ -1,68 +1,74 @@
+// canvas 
+var canvasWidth = 130  // width
+var canvasHeight = 48   // height
+var squareSize = 2
+var colorRandomFactor = 50
+var jumpFactor = 0.95
+px = parseInt(Math.floor(Math.random() * canvasWidth))
+py = parseInt(Math.floor(Math.random() * canvasHeight))
+hr = 0
+hg = 0
+hb = 0
+
 
 
 var net = require('net');
-var cw = 58
-var ch = 18
-var squareSize = 0.05
-var colorRandomFactor = 2
-var jumpFactor = 1.4
-
-
 var client = new net.Socket();
-client.connect(1234, '127.0.0.1', function() {
-  console.log('Connected');
-  px = parseInt(Math.random()*cw)
-  py = parseInt(Math.random()*ch)
-  hr = 0
-  hg = 0
-  hb = 0
-  setInterval(function(){
-    hr = hr + Math.round(colorRandomFactor-Math.random()*(colorRandomFactor*2))
-    if (hr >= 256) hr = 255;
-    if (hr < 0)   hr = 0;
-    var rr = hr.toString(16)
-    if (rr.length === 1) rr = '0'+rr;
-
-    hg = hg + Math.round(colorRandomFactor-Math.random()*(colorRandomFactor*2))
-    if (hg >= 255) hg = 255
-    if (hg < 0)   hg = 0
-    var rg = hg.toString(16)
-    if (rg.length === 1) rg = '0'+rg;
-
-    hb = hb + Math.round(colorRandomFactor-Math.random()*(colorRandomFactor*2))
-    if (hb >= 255) hb = 255
-    if (hb < 0)   hb = 0
-    var rb = hb.toString(16)
-    if (rb.length === 1) rb = '0'+rb;
-
-    for (var i=0; i<= squareSize; i++){
-      for (var k=0; k<= squareSize; k++){
-        var x = Math.floor(px+i)
-        var y = Math.floor(py+k)
-        var command = 'PX ' + x + ' ' + y + ' '+rr+rb+rg+'\n';
-        console.log(command)
-        client.write(command);
-      }
-    }
-    px = px + Math.round(1 - Math.random()*2)*squareSize*jumpFactor
-    py = py + Math.round(1 - Math.random()*2)*squareSize*jumpFactor
-    if (px > cw + squareSize) px = px - squareSize;
-    if (px < 0) px++;
-    if (py > ch + squareSize) py = py - squareSize;
-    if (py < 0) py++;
-    // console.log(command)
-  }, 0.001)
+client.connect(1234, 'localhost', function() {
+    console.log('Connected');
+    // random start positions
+    // render loop
+    setInterval(renderLoop, 10)
 });
 
 client.on('data', function(data) {
-  console.log('Received: ' + data);
-  client.destroy(); // kill client after server's response
+    console.log('Received: ' + data);
+    client.destroy(); // kill client after server's response
 });
 
 client.on('error', function(err) {
-   console.log(err)
-   client.destroy(); // kill client after server's response
+    console.log(err)
 })
 client.on('close', function() {
-  console.log('Connection closed');
+    console.log('Connection closed');
 });
+
+
+renderLoop = function() {
+    // change color..
+    hr = hr + Math.round(colorRandomFactor - Math.random() * (colorRandomFactor * 2))
+    if (hr >= 256) hr = 255;
+    if (hr < 0) hr = 0;
+    var rr = hr.toString(16)
+    if (rr.length === 1) rr = '0' + rr;
+
+    hg = hg + Math.round(colorRandomFactor - Math.random() * (colorRandomFactor * 2))
+    if (hg >= 255) hg = 255
+    if (hg < 0) hg = 0
+    var rg = hg.toString(16)
+    if (rg.length === 1) rg = '0' + rg;
+
+    hb = hb + Math.round(colorRandomFactor - Math.random() * (colorRandomFactor * 2))
+    if (hb >= 255) hb = 255
+    if (hb < 0) hb = 0
+    var rb = hb.toString(16)
+    if (rb.length === 1) rb = '0' + rb;
+
+    // draw square
+    for (var i = 0; i < squareSize; i++) {
+        for (var k = 0; k < squareSize; k++) {
+            var x = Math.round(px + i)
+            var y = Math.round(py + k)
+                //             PX     X         Y     BGCOLOR CHAR FGCOLOR █▓▒░┘
+            var command = 'PX ' + x + ' ' + y + ' ' + rr + rb + rg + ' / 000000\n';
+            client.write(command);
+        }
+    }
+    // move square
+    px = px + Math.round(1 - Math.random() * 2) * squareSize * jumpFactor
+    py = py + Math.round(1 - Math.random() * 2) * squareSize * jumpFactor
+    if (px > canvasWidth + squareSize) px = canvasWidth - squareSize;
+    if (py > canvasHeight) py = canvasHeight - squareSize;
+    if (px < 0) px++;
+    if (py < 0) py++;
+}
